68df6b58696c22ec849ed8b328f32899f93a8670
shmuel sternfeld <smoolzone@gmail.com>
2021-10-06 08:07:00 +0300

    feature first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartShown] = useState(false);
+
+  function showCartHandler() {
+    setCartShown(true);
+  }
+
+  function hideCartHandeler() {
+    setCartShown(false);
+  }
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
